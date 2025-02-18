105837f298021d471d7c89f10d0c0367b6f5002f
santiagosommer <s.sommerg@gmail.com>
2025-02-18 13:00:33 -0300

    Implemented react-hook-form for validating fields

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -2,20 +2,41 @@ import { Card } from './Card';
 import { Button } from "../components/Button";
 import EmailInput from "../components/EmailInput";
 import PasswordInput from "../components/PasswordInput";
+import { useForm, FormProvider } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import './LoginCard.css';
 
-export const LoginCard = () => (
-  <Card className="login-card mx-auto max-w-lg">
-    <div className="flex flex-col items-center justify-center p-4 space-y-4">
-      <h1 className="login-title text-2xl font-bold text-gray-900 dark:text-gray-50">Login</h1>
-      <p className="login-text font-bold">Welcome back!</p>
-      <div className="text-input w-full max-w-xs">
-        <EmailInput />
-        <PasswordInput />
-      </div>
-      <Button type="submit" className="login-button w-full max-w-xs" variant="primary">Log in</Button>
-    </div>
-  </Card>
-);
+const schema = yup.object().shape({
+  email: yup.string().email("Invalid email address").required("Email is required"),
+  password: yup.string().required("Password is required"),
+});
+
+export const LoginCard = () => {
+
+  const methods = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: { email: string; password: string }) => {
+    console.log(data.email, data.password);
+  };
+
+  return(
+    <FormProvider {...methods}>
+      <Card className="login-card mx-auto max-w-lg">
+        <form onSubmit={methods.handleSubmit(onSubmit)}  className="flex flex-col items-center justify-center p-4 space-y-4">
+          <h1 className="login-title text-2xl font-bold text-gray-900 dark:text-gray-50">Login</h1>
+          <p className="login-text font-bold">Welcome back!</p>
+          <div className="text-input w-full max-w-xs">
+            <EmailInput />
+            <PasswordInput />
+          </div>
+          <Button type="submit" className="login-button w-full max-w-xs" variant="primary">Log in</Button>
+        </form>
+      </Card>
+    </FormProvider>
+  );
+}
 
 export default LoginCard;
--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -1,18 +1,27 @@
 "use client"
 
+import { useFormContext } from "react-hook-form"
 import { Input } from "../components/Input"
 import { Label } from "./Label"
 
-export const PasswordInput = () => (
-  <div className="mx-auto max-w-xs space-y-2">
-    <Label htmlFor="password">Insert Password</Label>
-    <Input
-      className="w-full"
-      placeholder="Enter password"
-      type="password"
-      id="password"
-    />
-  </div>
-)
+export const PasswordInput = () => {
+  const { register, formState: { errors } } = useFormContext()
+
+  return(
+    <div className="mx-auto max-w-xs space-y-2">
+      <Label htmlFor="password">Insert Password</Label>
+      <Input
+        className="w-full"
+        placeholder="Enter password"
+        type="password"
+        id="password"
+        {...register("password", { required: "Password is required" })}
+      />
+      {errors.password?.message && typeof errors.password.message === 'string' && (
+        <p className="text-red-500 text-sm">{errors.password.message}</p>
+      )}
+    </div>
+  )
+}
 
 export default PasswordInput
