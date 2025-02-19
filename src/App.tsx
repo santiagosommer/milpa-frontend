fd874864cf214260a214c5fe66c896c6c57d7f1b
santiagosommer <s.sommerg@gmail.com>
2025-02-19 12:07:22 -0300

    div into fragment

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Layout from './components/Layout'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
