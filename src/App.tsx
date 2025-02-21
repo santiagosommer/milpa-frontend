048cd3b5cf7472e1e7727f42b7e5daf81c2947c8
santiagosommer <s.sommerg@gmail.com>
2025-02-21 09:42:21 -0300

    Added sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Layout from './components/Layout'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route index element={<Home />} />
         </Route>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,10 +9,10 @@ const Layout = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/blogs">Blogs</Link>
+            <Link to="/login">Login</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/signup">Sign Up</Link>
           </li>
         </ul>
       </nav>
